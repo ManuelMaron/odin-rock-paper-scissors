34c281b387997bd331cb8288332deba77de15d48
ManuelMaron <manuelmaron997@gmail.com>
2025-01-13 14:25:24 -0300

    Added function playRound checks humanChoice and computerChoice and selects a round winner

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 function getComputerChoice() {
     const choice = Math.floor(Math.random() * 3);
     if(choice === 0){
-        return "Paper!";
+        return "Paper"
     } else if (choice === 1){
-        return "Rock!";       
+        return "Rock"       
     } else {
-        return "Scissors!";
+        return "Scissors"
     }
 }
 
 function getHumanChoice() {
-    const humanChoice = prompt("What do you choose?");
-    return humanChoice + "!"
+    const humanChoice = prompt("Rock, Paper or Scissors?");
+    return humanChoice
 }
 
+const computerChoice = getComputerChoice();
+const humanChoice = getHumanChoice();
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if(humanChoice === computerChoice){
+        return console.log("Tie!");
+    } else if((humanChoice === "Rock" && computerChoice === "paper") || (humanChoice === "Paper" && computerChoice === "Scissors") || humanChoice === "Scissors" && computerChoice === "Rock"){
+        computerScore++
+        return console.log(`You Lose! ${computerChoice} beats ${humanChoice}`);
+    } else {
+        humanScore++
+        return console.log(`You Win! ${humanChoice} beats ${computerChoice}`);               
+    }
+}
+
+playRound(humanChoice, computerChoice);
